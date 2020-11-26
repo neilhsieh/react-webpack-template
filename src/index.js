f5d3f54e5a19901f3f1dbe05d553e5c93c3cbea2
neilhsieh <neilhsieh.94@gmail.com>
2020-11-25 21:10:39 -0500

    testing 123

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-ReactDom.render(<h1></h1>, document.getElementById("main-root"));
+ReactDom.render(<h1>Testing 123</h1>, document.getElementById("main-root"));
